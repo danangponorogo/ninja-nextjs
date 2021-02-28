857a0a7b8e7a762aca0aeb8b18d0fa6b9a222cb8
Danang Ponorogo <danang.dandies@gmail.com>
2021-02-28 12:17:11 +0700

    initial

---
+++ pages/about.js
@@ -0,0 +1,21 @@
+/*
+https://www.youtube.com/playlist?list=PL4cUxeGkcC9gZD-Tvwfod2gaISzfRiP9d
+https://www.youtube.com/watch?v=zktJ8-k0JDc&list=PL4cUxeGkcC9g9gP2onazU5-2M-AzA8eBw&index=2
+*/
+import Head from 'next/head'
+
+const About = () => {
+    return (
+        <div>
+            <Head>
+                <title>Ninja List | About</title>
+                <meta name="keywords" content="ninjas" />
+            </Head>
+            <h1>About</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita ducimus magni consequatur sint maiores doloremque repudiandae adipisci dignissimos reprehenderit saepe ipsum, dolorem laborum non laudantium vel, explicabo laboriosam distinctio eligendi?</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita ducimus magni consequatur sint maiores doloremque repudiandae adipisci dignissimos reprehenderit saepe ipsum, dolorem laborum non laudantium vel, explicabo laboriosam distinctio eligendi?</p>
+        </div>
+    );
+}
+
+export default About;
